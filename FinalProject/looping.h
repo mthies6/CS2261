a357415ee48bb091c328dbc117eef69029f1b639
Mackenzie Thies <dncrg@lawn-143-215-98-115.lawn.gatech.edu>
2023-10-20 12:46:14 -0400

    Cleaned Games
    Left only .c and .h files

--- FinalProject/looping.h
+++
@@ -1,5 +0,0 @@
-// looping sound made by wav2c
-
-extern const unsigned int looping_sampleRate;
-extern const unsigned int looping_length;
-extern const signed char looping_data[];
--- FinalProject/solve.h
+++
@@ -1,5 +0,0 @@
-// solve sound made by wav2c
-
-extern const unsigned int solve_sampleRate;
-extern const unsigned int solve_length;
-extern const signed char solve_data[];
